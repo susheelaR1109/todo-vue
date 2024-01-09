6b017dc40162d9e40fe4f220147dcb07bc4cfd30
susheelaR1109 <150230816+susheelaR1109@users.noreply.github.com>
2024-01-09 22:33:12 +0530

    add functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ new Vue({
     el:'#app',
     data: function(){
         return {
+            task:'',
             tasks: [
                 {
                     name:'Buy banana',
@@ -13,5 +14,16 @@ new Vue({
                 }
             ]
         }
+    },
+    methods: {
+        submitTask(){
+            // console.log(this.task)
+            if(this.task.length === 0) return;
+            this.tasks.push({
+                    name: this.task,
+                    status: 'to-do'
+                });
+            this.task=''
+        },
     }
 })
